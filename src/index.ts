5bae92a7063ab14aa70dd201ee77cd566f0f13cc
kuron106 <hirosi10893@gmail.com>
2020-12-31 15:16:24 +0900

    second commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import World from "./world";
 
 const root: HTMLElement | null  = document.getElementById('root');
-const world = new World("Hello TypeScript!");
+const world = new World("Hello TypeScript!?");
 world.sayHello(root);
